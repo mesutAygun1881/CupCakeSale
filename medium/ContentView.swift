23d033bc4dfed00f59b5a2e9c01d58cc5640ad5a
mesut aygun <mesutaygun35@gmail.com>
2021-05-18 18:00:35 +0300

    Initial Commit

---
+++ medium/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  medium
+//
+//  Created by Mesut Aygün on 18.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ medium/mediumApp.swift
@@ -0,0 +1,17 @@
+//
+//  mediumApp.swift
+//  medium
+//
+//  Created by Mesut Aygün on 18.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct mediumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
